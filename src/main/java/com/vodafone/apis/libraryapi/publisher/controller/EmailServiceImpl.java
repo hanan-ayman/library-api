1e67f2f1fe989c678234e939483060b83b56ae78
hanan ayman <hanan.ayman.said@gmail.com>
2021-01-10 01:03:36 +0200

    sending email with attachement

--- src/main/java/com/vodafone/apis/libraryapi/publisher/controller/EmailServiceImpl.java
+++ src/main/java/com/vodafone/apis/libraryapi/publisher/controller/EmailServiceImpl.java
@@ -3,7 +3,6 @@ package com.vodafone.apis.libraryapi.publisher.controller;
 import com.vodafone.apis.libraryapi.publisher.controller.email.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -18,15 +17,14 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public void sendSimpleMessage(
             String to, String subject, String text) throws MessagingException {
-        //TODO: This is a simple way to send email without attachment
+        //This is a simple way to send email without attachment
 //        SimpleMailMessage message = new SimpleMailMessage();
 //        message.setFrom("hanan.ayman.said@gmail.com");
 //        message.setTo(to);
 //        message.setSubject(subject);
 //        message.setText(text);
 //        emailSender.send(message);
-
-//TODO: This is a simple way to send email with attachment
+        //This is a simple way to send email with attachment
         MimeMessage msg = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(msg, true);
         helper.setTo(to);
